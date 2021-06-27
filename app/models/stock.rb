c355621043767a0f31a7a94e9029ef2dd4951f99
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-06-27 22:20:26 +0100

    Reconfigure Stock model return new object

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,6 +5,6 @@ class Stock < ApplicationRecord
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
 
-    client.price(ticker_symbol)
+    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
   end
 end
