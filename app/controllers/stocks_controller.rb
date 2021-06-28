cfda72047b6c2a00d2cc1f797e255da15a178b84
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-06-28 09:35:16 +0100

    Display error for invalid symbol

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,7 +3,12 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
-      render 'users/my_portfolio'
+      if @stock.nil?
+        flash[:alert] = "Please enter a valid symbol"
+        redirect_to my_portfolio_path
+      else
+        render 'users/my_portfolio'
+      end
     else
       flash[:alert] = "Please enter a symbol to search"
       redirect_to my_portfolio_path
