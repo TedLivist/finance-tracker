73f2e74fcd6091482f0384d603ac2e38b81729a9
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-10-18 21:06:34 +0100

    Create 'Add to portfolio' button and allow create route path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_stocks
+  resources :user_stocks, only: [:create]
   root 'welcome#index'
   devise_for :users
   get 'my_portfolio', to: 'users#my_portfolio'
