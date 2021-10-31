45b234dd2aaf8f435a41e249ae3a7fe19ae8ddb5
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-10-31 13:47:08 +0100

    Complete tracking stocks functions in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,17 @@ class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
 
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false unless stock
+    stocks.where(id: stock.id).exists?
+  end
 
   def under_stock_limit?
     stocks.count < 10
   end
 
   def can_track_stock?(ticker_symbol)
-    under_stock_limit?
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)
   end
 end
