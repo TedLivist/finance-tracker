9192095b7a78de55061411d996087d57ebd8fbae
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-10-31 16:02:01 +0100

    Implement deletion at controller level

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: [:create, :destroy]
   root 'welcome#index'
   devise_for :users
   get 'my_portfolio', to: 'users#my_portfolio'
