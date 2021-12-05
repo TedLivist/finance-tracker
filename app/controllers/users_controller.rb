316749a34221adbacc6112ced75d5201fe4734de
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-12-05 02:24:30 +0100

    Implement friend search action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,23 @@ class UsersController < ApplicationController
   end
 
   def search
-    render json: params[:friend]
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+         flash.now[:alert] = "Could not find user"
+         format.js { render partial: 'users/friend_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to search"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
   end
 end
