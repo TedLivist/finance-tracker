ce4ed2ebe147759fa8843a65ea65b044e2aec3da
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-12-19 22:06:53 +0100

    Implement destroy friendship destroy

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,5 +3,9 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
+    friendship = current_user.friendships.where(friend_id: params[:id]).first
+    friendship.destroy
+    flash[:notice] = "Stopped following"
+    redirect_to my_friends_path
   end
 end
