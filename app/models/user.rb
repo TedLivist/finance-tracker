e5442af78bf915993b8b00a3ee7007f196be21c5
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-12-19 09:34:19 +0100

    Implement user search functionality in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,28 @@ class User < ApplicationRecord
       "Anonymous"
     end
   end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
 end
